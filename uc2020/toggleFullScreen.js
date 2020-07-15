b92417e5999ba47db022c8a2708c45cad8acbbcd
Nicholas Furness <nfurness@esri.com>
2020-07-15 12:49:36 -0400

    Prettfy the JS source

--- uc2020/toggleFullScreen.js
+++ uc2020/toggleFullScreen.js
@@ -1,7 +1,7 @@
 (() => {
-    if (window.toggleFullscreenChat !== undefined) { 
-    window.toggleFullscreenChat();
-    return;
+    if (window.toggleFullscreenChat !== undefined) {
+        window.toggleFullscreenChat();
+        return;
     }
     const css = '.fullscreen-chat .live-stream-plugin-container {height:100vh !important;position:fixed;z-index:9999;width:100vw;top:0;left:0;margin:0;} .fullscreen-chat {overflow:hidden;} .dark.str-chat .long {background:darkred !important}';
     const head = document.head || document.getElementsByTagName('head')[0];
@@ -19,22 +19,22 @@
     document.querySelector('.rta.str-chat__textarea').appendChild(charCount);
 
     window.toggleFullscreenChat = () => {
-    if (document.body.classList.contains('fullscreen-chat')) {
-    document.body.classList.remove('fullscreen-chat');
-    } else {
-    document.body.classList.add('fullscreen-chat');
-    }
+        if (document.body.classList.contains('fullscreen-chat')) {
+            document.body.classList.remove('fullscreen-chat');
+        } else {
+            document.body.classList.add('fullscreen-chat');
+        }
     }
     document.body.addEventListener('click', () => {
-    const checkLength = (e) => {
-        document.querySelector('#nixta-hack-charcount').innerHTML = `${e.target.value.length}/280`
-        if (e.target.value.length > 280) {
-            document.querySelector('.str-chat textarea').classList.add('long');
+        const checkLength = (e) => {
+            document.querySelector('#nixta-hack-charcount').innerHTML = `${e.target.value.length}/280`
+            if (e.target.value.length > 280) {
+                document.querySelector('.str-chat textarea').classList.add('long');
             } else {
-            document.querySelector('.str-chat textarea').classList.remove('long');
-        }        
-    }
-    document.querySelector('.str-chat textarea').addEventListener('keyup', checkLength)
-    document.querySelector('.str-chat textarea').addEventListener('paste', checkLength)
+                document.querySelector('.str-chat textarea').classList.remove('long');
+            }
+        }
+        document.querySelector('.str-chat textarea').addEventListener('keyup', checkLength)
+        document.querySelector('.str-chat textarea').addEventListener('paste', checkLength)
     });
-    })()
+})()
